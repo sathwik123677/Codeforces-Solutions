4971d6476c5f7b4f9879960705291508ac2b12f5
sathwik123677 <sathwikp06@gmail.com>
2026-08-03 11:41:29 +0530

    Added one problem

---
+++ Aug-2026/1260A.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using int64 = long long;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    while (n--) {
+        int64 c, sum;
+        cin >> c >> sum;
+
+        int64 k = min(c, sum);
+
+        int64 q = sum / k;
+        int64 r = sum % k;
+
+        int64 ans = (k - r) * q * q + r * (q + 1) * (q + 1);
+
+        cout << ans << "\n";
+    }
+}
